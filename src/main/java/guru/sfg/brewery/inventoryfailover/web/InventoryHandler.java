693a95355a4f2a78a7f1506e8bf633d5c678c7a2
jasonc-dev <jasoncohen07@gmail.com>
2022-01-07 23:32:21 +0000

    updating to return list of invDTO objects

--- src/main/java/guru/sfg/brewery/inventoryfailover/web/InventoryHandler.java
+++ src/main/java/guru/sfg/brewery/inventoryfailover/web/InventoryHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
 import java.time.OffsetDateTime;
+import java.util.Arrays;
 import java.util.UUID;
 
 /**
@@ -18,12 +19,12 @@ public class InventoryHandler {
     public Mono<ServerResponse> listInventory(ServerRequest request){
         return ServerResponse.ok()
                 .contentType(MediaType.APPLICATION_STREAM_JSON)
-                .body(Mono.just(BeerInventoryDto.builder()
+                .body(Mono.just(Arrays.asList(BeerInventoryDto.builder()
                     .id(UUID.randomUUID())
                     .beerId(UUID.fromString("00000000-0000-0000-0000-000000000000"))
                     .quantityOnHand(999)
                     .createdDate(OffsetDateTime.now())
                     .lastModifiedDate(OffsetDateTime.now())
-                        .build()), BeerInventoryDto.class);
+                        .build())), BeerInventoryDto.class);
     }
 }
